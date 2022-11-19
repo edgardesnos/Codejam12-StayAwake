17eb1b088988284bdc95c60de4bc60ef2c732615
triantosnick1122 <nicholas.triantos@cs.mcgill.ca>
2022-11-19 12:52:40 -0500

    code cleanup

--- backend/src/loginController.py
+++ backend/src/loginController.py
@@ -4,28 +4,16 @@ import pyodbc
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World'
-
-@app.route("/poop")
-def poop():
-    return 'Poop.'
-
-
 # @app.route('/login', methods = ['POST'])
 @app.route('/login/<username>/<password>')
 def confirm_login(username, password):
-    # username = request.args.get('username')
-    # password = request.args.get('password')
 
     # check if the username and password combination is valid in the db
     print("SELECT * from Users where uname = '" + username + "' AND pass = '" + password + "'")
     results = select_query("SELECT * from Users where uname = '" + username + "' AND pass = '" + password + "'")
     # if 1 row returned, then we logged in successfully
     count = len(results)
-    if (count >= 1):
+    if (count == 1): # username is unique in db, so not going to be more than 1
         return "Successful Login!"
     else:
         return "Login failed."
