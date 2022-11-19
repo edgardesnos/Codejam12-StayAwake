92ad5355652d527c5b60be58846d2ae22c6bcc32
triantosnick1122 <nicholas.triantos@cs.mcgill.ca>
2022-11-19 13:39:04 -0500

    new endpoint for reporting drowsiness incidents, also an example json to format such requests easily

--- backend/src/loginController.py
+++ backend/src/loginController.py
@@ -4,6 +4,20 @@ import pyodbc
 
 app = Flask(__name__)
 
+@app.route('/incidents/report', methods = ['POST'])
+def report_incident():
+    json = request.json
+    x = json['x']
+    y = json['y']
+    address = json['address'] # probably should look up the address using coordinates instead of passing it here
+    time = json['time']
+
+    # create a new incident report in the database
+    insert_statement = "INSERT INTO Drowsiness_Report(gps_x_cord, gps_y_cord, time, incident_address) VALUES (" + x + ", " + y + ", '" +  time + "' , '" + address + "')"
+    insert(insert_statement)
+    return "Inserted into database" # TODO: error checking? this return value is not helpful
+
+
 # @app.route('/login', methods = ['POST'])
 @app.route('/login/<username>/<password>')
 def confirm_login(username, password):
@@ -26,8 +40,21 @@ def select_query(query):
     connection = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
     cursor = connection.cursor() # the actual object we use to query
     cursor.execute(query)
+    results = cursor.fetchall()
+    connection.close()
     # return all records
-    return cursor.fetchall()
+    return results
+
+# TODO: return something
+def insert(statement):
+    server = 'codejam12-sql-server.database.windows.net'
+    database = 'codejam'
+    username = 'nick'
+    password = 'FuozZy4DK'
+    connection = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+    cursor = connection.cursor() # the actual object we use to query
+    cursor.execute(statement)
+    connection.close()
 
 if __name__ == '__main__':
 
