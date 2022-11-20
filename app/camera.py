c67ad2a7dae1463318228352a60133cb7afc6691
edgardesnos <edgar.desnos@mail.mcgill.ca>
2022-11-19 21:23:13 -0500

    Add face mesh

--- app/camera.py
+++ app/camera.py
@@ -1,16 +1,19 @@
 import cv2
 from face_detector import FaceDetector
+from face_mesh import FaceMeshDetector
 
 class VideoCamera(object):
     def __init__(self):
        self.video = cv2.VideoCapture(2)
        self.face_detector = FaceDetector()
+       self.face_mesh_detector = FaceMeshDetector()
     
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
         ret, frame = self.video.read()
-        frame = self.face_detector.detect_face(frame)
+        #frame = self.face_detector.detect_face(frame)
+        frame = self.face_mesh_detector.detect_mesh(frame)
         ret, jpeg = cv2.imencode(".jpg", frame)
         return jpeg.tobytes()
---
+++ app/face_mesh.py
@@ -0,0 +1,50 @@
+import mediapipe as mp
+import cv2
+
+class FaceMeshDetector:
+
+    def __init__(self):
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_drawing_styles = mp.solutions.drawing_styles
+        self.mp_face_mesh = mp.solutions.face_mesh
+        self.face_mesh = self.mp_face_mesh.FaceMesh(
+            max_num_faces=1,
+            refine_landmarks=True,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+        )
+
+    def detect_mesh(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = self.face_mesh.process(image)
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                self.mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=self.mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=self.mp_drawing_styles
+                    .get_default_face_mesh_tesselation_style()
+                )
+                self.mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=self.mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=self.mp_drawing_styles
+                    .get_default_face_mesh_contours_style()
+                )
+                self.mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=self.mp_face_mesh.FACEMESH_IRISES,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=self.mp_drawing_styles
+                    .get_default_face_mesh_iris_connections_style()
+                )
+        
+        return image
+
