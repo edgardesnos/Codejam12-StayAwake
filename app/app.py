3fd1028965f7e68a0c369988095e22d78126c0a6
Arman Shroff-Mehrabadi <arman.shroff-mehrabadi@mail.mcgill.ca>
2022-11-19 22:03:55 -0500

    login page added (static)

--- app/app.py
+++ app/app.py
@@ -4,24 +4,14 @@ import jyserver.Flask as jsf
 import requests, json, geocoder
 app = Flask(__name__)
 
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
 
-@jsf.use(app) # Connect Flask object to JyServer; used to get location
-class App:
-    coordinates = []
-    def __init__(self):
-        self.coordinates = [0, 0]
-
-    def get_address(self):
-        lat = self.js.document.getElementById("latitude").innerHTML
-        long = self.js.document.getElementById("longitude").innerHTML
-        return [lat, long]
-        #self.coordinates = [lat, long]
 @app.route("/") # base URL (home page, first time being run)
 def index():
-    return App.render(render_template("index.html", address="loading..."))
-
-@app.route("/home") #  (home page after address loads)
-def home():
     g = geocoder.ip('me')
     coords = g.latlng
     lat = coords[0]
@@ -30,9 +20,11 @@ def home():
     response = requests.get(url)
     data = response.json() 
     addr = data["resourceSets"][0]["resources"][0]["addressOfLocation"][0]["formattedAddress"]
-    # rendering webpage
-    return App.render(render_template("index.html", address=addr))
+    return render_template("index.html", address=addr)
 
+@app.route("/login") # base URL (home page, first time being run)
+def login(): 
+    return render_template("login.html")
 # def get_address(): 
 #     #coordinates = App.get_coords()
 #     #url = f"https://dev.virtualearth.net/REST/v1/LocationRecog/{coordinates[0]},{coordinates[1]}?&top=1&includeEntityTypes=address&key=As121dM81pVFCIUxLtVB3xYQ-ps7x7jCP7PlEFfvI4RM87V4OUkHV4h3lEpDGeUW"
